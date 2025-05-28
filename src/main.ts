a5bb0018817926f24e999fde4daf8d056c52ede7
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-05-28 09:11:58 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+// configures dotenv to work in your application
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (request: Request, response: Response) => {
+    response.status(200).json({ "status": "api works" });
+});
+
+app.listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+}).on("error", (error) => {
+    // gracefully handle error
+    throw new Error(error.message);
+});
